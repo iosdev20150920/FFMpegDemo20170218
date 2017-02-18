7c20656b570b17f4c2bcfd72dd75a67d45c1235e
iosdev20150920 <iosdev20150920@126.com>
2017-02-18 13:15:50 +0800

    01.提交第一次变更

---
+++ FFMpegDemo20170218/FFMpegDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFMpegDemo
+//
+//  Created by yangfs on 2017/2/18.
+//  Copyright © 2017年 yangfs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFMpegDemo20170218/FFMpegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFMpegDemo
+//
+//  Created by yangfs on 2017/2/18.
+//  Copyright © 2017年 yangfs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
